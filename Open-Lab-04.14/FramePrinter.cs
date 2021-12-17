621bf94e7b903a1e1284424640fcb36a76bff085
Andrej Svrček <nepoviem251@gmail.com>
2021-12-17 10:25:16 +0100

    Prints strings into a console each to separate line in a frame

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +7,39 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            string longest = strings.OrderByDescending(s => s.Length).First();
+
+            string a = "";
+            string b = "";
+
+            for (int i = 0; i < longest.Length + 4; i++)
+            {
+                a += "*";
+            }
+            Console.WriteLine($"{a}");
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length == longest.Length)
+                {
+                    Console.WriteLine($"* {strings[i]} *");
+                }
+                else
+                {
+                    for (int j = 0; strings[i].Length < longest.Length; j++)
+                    {
+                        strings[i] += " ";
+                    }
+                    Console.WriteLine($"* {strings[i]} *");
+                }
+            }
+
+            for (int i = 0; i < longest.Length + 4; i++)
+            {
+                 b += "*";
+            }
+            Console.WriteLine($"{b}");
         }
+
     }
 }
